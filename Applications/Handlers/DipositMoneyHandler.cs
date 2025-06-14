3631561cc918c6428522247ae7a26b04a4f2e60f
mohannad balam <b.mohanned85@gmail.com>
2025-06-14 21:44:51 +0200

    add the rest of command handlers

---
+++ Applications/Handlers/DipositMoneyHandler.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Aggregates;
+using EventSourcedBankAccountManagement.Domain.Commands;
+using EventSourcedBankAccountManagement.Infrastructure.EventStore;
+
+namespace EventSourcedBankAccountManagement.Applications.Handlers
+{
+    public class DipositMoneyHandler(IEventStore store)
+    {
+        private readonly IEventStore _store = store;
+
+        public async Task Handle(DepositMoneyCommand cmd)
+        {
+            var history = await _store.LoadStream(cmd.AccountId);
+            var acct = new Account(history);
+            acct.Deposit(cmd.Amount);
+            await _store.AppendToStream(cmd.AccountId, acct.UncommittedEvents);
+        }
+    }
+}
+
---
+++ Applications/Handlers/WithdrawMoneyHandler.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Aggregates;
+using EventSourcedBankAccountManagement.Domain.Commands;
+using EventSourcedBankAccountManagement.Infrastructure.EventStore;
+
+namespace EventSourcedBankAccountManagement.Applications.Handlers
+{
+    public class WithdarwMoneyHandler(IEventStore store)
+    {
+        private readonly IEventStore _store = store;
+
+        public async Task Handle(WithdrawMoneyCommand cmd)
+        {
+            var history = await _store.LoadStream(cmd.AccountId);
+            var acct = new Account(history);
+            acct.Withdraw(cmd.Amount);
+            await _store.AppendToStream(cmd.AccountId, acct.UncommittedEvents);
+        }
+    }
+}
